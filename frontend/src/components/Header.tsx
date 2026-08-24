3a9f265e9ac0dcbc703b42fda6cadbeb1b016382
Pozytron <stetsenko.vitaliy@gmail.com>
2026-08-24 22:34:42 +0200

    style: update header and root styles for improved layout and responsiveness

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -36,8 +36,7 @@ export const Header = () => {
           <>
             {isAdmin && (
               <Link to="/admin" className='header-icon-button' style={{marginRight: '1.5rem'}}>
-                <svg width="24" height="24" style={{ width: '34px',
-                height: '34px', color: '#2ca7e6'}} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M12 2.75L19.25 5.5V11C19.25 15.75 16.25 19.5 12 21.25C7.75 19.5 4.75 15.75 4.75 11V5.5L12 2.75Z" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
                   <path d="M9 11.25L11 13.25L15.25 9" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
@@ -45,15 +44,13 @@ export const Header = () => {
             )}
             
             <Link to="/profile"  className='header-icon-button'>
-              <svg width="24" height="24" style={{ width: '34px',
-                height: '34px', color: '#9530d8'}} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="12" cy="8" r="3.25" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
-              <path d="M5.5 20V18.5C5.5 15.74 7.74 13.5 10.5 13.5H13.5C16.26 13.5 18.5 15.74 18.5 18.5V20" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="8" r="3.25" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M5.5 20V18.5C5.5 15.74 7.74 13.5 10.5 13.5H13.5C16.26 13.5 18.5 15.74 18.5 18.5V20" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
             </Link>
             <Link to="/settings"  className='header-icon-button'>
-              <svg width="24" height="24" style={{ width: '34px',
-                height: '34px', color: '#628597'}} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
+              <svg width="24" height="24"  viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4.5 6.5H14" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round"/>
                 <path d="M17.25 6.5H19.5" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round"/>
                 <circle cx="15.75" cy="6.5" r="1.75" stroke="currentColor" strokeWidth="1.75"/>
@@ -68,18 +65,16 @@ export const Header = () => {
               </svg>
             </Link>
             <Link to="/rating"  className='header-icon-button'>
-              <svg width="24" height="24" style={{ width: '34px',
-                height: '34px', color: '#ffd700'}} viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path d="M6 17v-5"/>
-  <path d="M12 17v-9"/>
-  <path d="M18 17v-3"/>
-  <path d="M4 17h16"/>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"> 
+              <path d="M9 5H15V15H9V5Z" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M3.5 9H9V15H3.5V9Z" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M15 11H20.5V15H15V11Z" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M3.5 15H20.5V19H3.5V15Z" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
             </Link>
 
-            <button onClick={handleLogout}  className='header-icon-button' style={{cursor: 'pointer', background: 'transparent' , border: '0'}}>
-              <svg width="24" height="24" style={{ width: '34px',
-                height: '34px', color: '#db4d4d'}} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <button onClick={handleLogout}  className='header-icon-button' style={{}}>
+              <svg width="24" height="24"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M10.5 20H6.5C5.4 20 4.5 19.1 4.5 18V6C4.5 4.9 5.4 4 6.5 4H10.5" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M15 16.25L19.25 12L15 7.75" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M19 12H9.5" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
